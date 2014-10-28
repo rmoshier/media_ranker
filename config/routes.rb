7238438a81ad752374ff4e824bd169aaab2bde85
Rachel <rmoshier@gmail.com>
2014-10-28 16:50:52 -0700

    added books controller, views and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
 
   get 'home/index'
-  get '/movies', to: "movies#index"
+  get '/movies',      to: "movies#index"
+
+  get '/books',       to: "books#index"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
