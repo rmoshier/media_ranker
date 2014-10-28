6190ce8ba01df10f5db22dc4ffeb30eca249967f
Rachel <rmoshier@gmail.com>
2014-10-28 16:53:30 -0700

    added albums controller, views and route

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,4 +1,4 @@
 class AlbumsController < ApplicationController
-
-
+  def index
+  end  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
 
   get '/books',       to: "books#index"
 
+  get '/albums',      to: "albums#index"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
